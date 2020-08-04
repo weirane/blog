5fb2b5fd0963987d5ba5d5733b42cf3ee004f2e7
Wang Ruochen <wangkevin920@163.com>
2020-08-04 23:19:16 +0800

    Fix travis commit message
    %s - subject
    %b - body

--- script/travis.sh
+++ script/travis.sh
@@ -12,6 +12,6 @@ git init
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 git add --all
-git commit --message "Auto deploy from Travis CI build $TRAVIS_BUILD_NUMBER" --message "$(git -C .. log -1 --pretty="[%h] %b")"
+git commit --message "Auto deploy from Travis CI build $TRAVIS_BUILD_NUMBER" --message "$(git -C .. log -1 --pretty="[%h] %s")"
 git remote add deploy "https://$GH_TOKEN@github.com/weirane/weirane.github.io.git" >/dev/null 2>&1
 git push --force deploy master
