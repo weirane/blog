872a9bb6ec8c7b1b31d73dab65c15b9828f5b3cc
Wang Ruochen <wrc@ruo-chen.wang>
2024-07-04 11:45:22 -0400

    fix workflow

--- script/github.sh
+++ script/github.sh
@@ -9,5 +9,5 @@ git config user.email "noreply@github.com"
 git add --all
 git commit --message "Auto deploy from GitHub Actions build $GITHUB_RUN_NUMBER" \
            --message "$(git -C .. log -1 --pretty="[%h] %s")"
-git remote add deploy "https://weirane:$GH_TOKEN@github.com/weirane/weirane.github.io.git" >/dev/null 2>&1
+git remote add deploy "https://$GH_TOKEN@github.com/weirane/weirane.github.io.git" >/dev/null 2>&1
 git push --force deploy master
